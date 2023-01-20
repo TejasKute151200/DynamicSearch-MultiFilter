c158420099c3a267888ee2747bc8b9b3757e4c14
TejasKute151200 <113657741+TejasKute151200@users.noreply.github.com>
2023-01-20 17:02:04 +0530

    Add files via upload

--- spring-boot-multifilter-example/src/main/java/com/multifilter/dto/EmployeeDto.java
+++ spring-boot-multifilter-example/src/main/java/com/multifilter/dto/EmployeeDto.java
@@ -1,5 +1,6 @@
 package com.multifilter.dto;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -26,6 +27,7 @@ public class EmployeeDto {
 	@NotNull(message = "salary Can Not Be Null*salaryRequired")
 	private double salary;
 
+	@Email(message = "Email is Not Valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
 	@NotEmpty(message = "Email Can Not Be Null Or Empty*emailRequired")
 	@NotBlank(message = "Email Can Not Be Null Or Blank*emailRequired")
 	private String email;
